4dbcb945fb79b021bfc158e5fca323bd31596dc1
Mehdix <mehdi.benchikh1@gmail.com>
2025-06-20 16:08:11 +0100

    last version

--- api/index.py
+++ api/index.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, flash, request
 from flask_bootstrap import Bootstrap5
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
@@ -18,13 +18,19 @@ static_dir = os.path.abspath(os.path.join(base_dir, '../static'))
 # Load environment variables
 load_dotenv()
 SECRET_KEY = os.getenv("SECRET_KEY")
-DATABASE_URI = os.getenv("DATABASE_URI")
+TMDB_API_KEY = os.getenv("TMDB_API_KEY")
+TMDB_URL = os.getenv("TMDB_URL")
 
+# In deployement
 app = Flask(__name__, template_folder=template_dir, static_folder=static_dir, instance_path="/tmp")
-app.config['SECRET_KEY'] = SECRET_KEY
-app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
-Bootstrap5(app)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///tmp/movies.db"
 
+# In development
+# app = Flask(__name__, template_folder=template_dir, static_folder=static_dir)
+# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///movies.db"
+
+app.config['SECRET_KEY'] = SECRET_KEY
+Bootstrap5(app)
 current_year = datetime.now().year
 
 # CREATE DB
@@ -45,60 +51,61 @@ class Movie(db.Model):
     review: Mapped[str] = mapped_column(String(250))
     img_url: Mapped[str] = mapped_column(String(250))
 
-movies = [
-    Movie(
-        title="The Dark Knight",
-        year=2008,
-        description="When the menace known as the Joker wreaks havoc and chaos on Gotham, Batman must accept one of the greatest psychological and moral tests of his ability to fight injustice.",
-        rating=10,
-        ranking=1,
-        review="Heath Ledger's Joker redefined comic book villains. Dark, intense, and unforgettable.",
-        img_url="static/img/the_dark_knight.jpg"
-    ),
-    Movie(
-        title = "Inception",
-        year = 2010,
-        description = "A skilled thief is given a chance at redemption if he can successfully perform an inception — planting an idea into a target's subconscious.",
-        rating=8.5,
-        ranking=5,
-        review = "Mind-bending and visually stunning. A masterpiece by Nolan.",
-        img_url = "static/img/inception.jpg"
-    ),
-    Movie(
-        title="Interstellar",
-        year=2014,
-        description="A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
-        rating=9.2,
-        ranking=3,
-        review="Emotionally powerful and scientifically ambitious.",
-        img_url="static/img/interstellar.jpg"
-    ),
-    Movie(
-        title="The Shawshank Redemption",
-        year=1994,
-        description="Two imprisoned men bond over years, finding solace and redemption through acts of common decency.",
-        rating=9.5,
-        ranking=2,
-        review="A timeless story of hope and humanity.",
-        img_url="static/img/the_shawshank_redemption.jpg"
-    ),
-    Movie(
-        title="The Godfather",
-        year=1972,
-        description="The aging patriarch of an organized crime dynasty transfers control to his reluctant son.",
-        rating=9.0,
-        ranking=4,
-        review="Flawless acting and storytelling. A cinematic legend.",
-        img_url="static/img/the_godfather.jpg"
-    )
-]
 
+def add_default_movies():
+    db.session.add_all([
+        Movie(
+            title="The Dark Knight",
+            year=2008,
+            description="When the menace known as the Joker wreaks havoc...",
+            rating=10,
+            ranking=1,
+            review="Heath Ledger's Joker redefined comic book villains.",
+            img_url="static/img/the_dark_knight.jpg"
+        ),
+        Movie(
+            title="Inception",
+            year=2010,
+            description="A skilled thief is given a chance at redemption...",
+            rating=8.5,
+            ranking=5,
+            review="Mind-bending and visually stunning. A masterpiece.",
+            img_url="static/img/inception.jpg"
+        ),
+        Movie(
+            title="Interstellar",
+            year=2014,
+            description="A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
+            rating=9.2,
+            ranking=3,
+            review="Emotionally powerful and scientifically ambitious.",
+            img_url="static/img/interstellar.jpg"
+        ),
+        Movie(
+            title="The Shawshank Redemption",
+            year=1994,
+            description="Two imprisoned men bond over years, finding solace and redemption through acts of common decency.",
+            rating=9.5,
+            ranking=2,
+            review="A timeless story of hope and humanity.",
+            img_url="static/img/the_shawshank_redemption.jpg"
+        ),
+        Movie(
+            title="The Godfather",
+            year=1972,
+            description="The aging patriarch of an organized crime dynasty transfers control to his reluctant son.",
+            rating=9.0,
+            ranking=4,
+            review="Flawless acting and storytelling. A cinematic legend.",
+            img_url="static/img/the_godfather.jpg"
+        )
+    ])
+    db.session.commit()
+
+# execute only the first time
 with app.app_context():
     db.create_all()
-
-    for movie in movies:
-        db.session.add(movie)
-    db.session.commit()
+    add_default_movies()
 
 # Edit Form
 class MovieForm(FlaskForm):
@@ -107,13 +114,14 @@ class MovieForm(FlaskForm):
     description = wtf.TextAreaField("Description", validators=[DataRequired(), Length(max=250)])
     rating = wtf.FloatField("Rating", validators=[InputRequired(), NumberRange(min=0.0, max=10,)])
     ranking = wtf.IntegerField("Ranking", validators=[DataRequired(), NumberRange(min=1)])
-    review = wtf.TextAreaField("Review", validators=[DataRequired(), Length(max=250)])
+    review = wtf.TextAreaField("Review", validators=[DataRequired(), Length(max=70)])
     img_url = wtf.StringField("Image URL", validators=[DataRequired()])
     submit = wtf.SubmitField("Save")
 
 class AddMovieForm(FlaskForm):
     title = wtf.StringField("Movie Title", validators=[DataRequired()])
-    submit = wtf.SubmitField("Save")
+    submit = wtf.SubmitField("Search")
+
 
 @app.route("/")
 def home():
@@ -135,6 +143,7 @@ def edit(movie_id):
             movie.review = form.review.data
             movie.img_url = form.img_url.data
             db.session.commit()
+            flash("Movie updated successfully", "info")
             return redirect(url_for("home"))
     return render_template("edit.html", form=form, movie=movie)
 
@@ -144,6 +153,7 @@ def delete(movie_id):
     movie_to_delete = db.get_or_404(Movie, movie_id)
     db.session.delete(movie_to_delete)
     db.session.commit()
+    flash("Movie deleted", "danger")
     return redirect(url_for('home'))
 
 
@@ -152,9 +162,47 @@ def add():
     form = AddMovieForm()
     if request.method == 'GET':
         return render_template("add.html", form=form)
-    if form.validate_on_submit():
-        
+    else:
+        if form.validate_on_submit():
+            params = {
+                "query": form.data.get("title"),
+                "api_key": TMDB_API_KEY,
+                "language": "en-US"
+            }
+            response = requests.get(TMDB_URL, params=params)
+            data = response.json()
+            results = data["results"]
+            if not results:
+                flash("Movie Not Found", "danger")
+                return render_template("add.html",form=form)  
+            return render_template("select.html", movies=results)
+
+
+@app.route("/select", methods=['GET', 'POST'])
+def select():
+    if request.method == "POST":
+        new_movie = Movie(
+            title = request.form.get("title"),
+            year = int(request.form.get("year")),
+            description = request.form.get("overview"),
+            rating = float(request.form.get("rating")),
+            ranking = Movie.query.count() + 1,
+            review = "loved it!",
+            img_url = request.form.get("img_url")
+        )
+        db.session.add(new_movie)
+        db.session.commit()
+        flash("Movie added successfully", "primary")
         return redirect(url_for("home"))
+
+
+@app.route("/reset")
+def reset():
+    db.session.query(Movie).delete()
+    add_default_movies()
+    flash("Movie list has been reset", "success")
+    return redirect(url_for("home"))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
